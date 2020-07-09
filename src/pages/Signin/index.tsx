9430fd9df9829713b43105ed0d96464724429252
AlessandroBorges41 <alessandroasb@gmail.com>
2020-07-08 23:18:50 -0300

    Implementation of Toast and Routes

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -5,7 +5,7 @@ import { FormHandles } from "@unform/core";
 import { Form } from "@unform/web";
 import * as Yup from 'yup';
 
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
 import { useAuth } from '../../hooks/auth';
 import { useToast } from '../../hooks/toast';
@@ -35,6 +35,7 @@ const SignIn: React.FC = () => {
 
     const { signIn } = useAuth();
     const { addToast } = useToast();
+    const history = useHistory();
 
     //console.log(user);
 
@@ -56,6 +57,9 @@ const SignIn: React.FC = () => {
                 email: data.email,
                 password: data.password
             });
+
+            history.push('/dashboard');
+
         } catch (err) {
             console.log(err);
             /* Verificando se o erro gerado é uma instaância do YUP */
@@ -77,7 +81,7 @@ const SignIn: React.FC = () => {
             });
         }
     },
-     [signIn, addToast],
+     [signIn, addToast, history],
     );
 
     return(
